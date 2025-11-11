6efa6ff86c6fd6671b35da881e4d7e4df1972e97
Luis González <ljgonzalez@uc.cl>
2025-11-11 11:22:43 -0300

    🔧 fix(include/bds/array/bds_array.h): update type from 'ConstArray' to 'const Array' for consistency and clarity

--- include/bds/array/bds_array.h
+++ include/bds/array/bds_array.h
@@ -8,8 +8,6 @@ typedef struct bds_array {
     size_t length;
 } Array;
 
-typedef const Array *ConstArray;
-
 // Key functions
 typedef int (*key_val_func)(const void *elem);
 typedef void (*deleter_func)(void *elem);
@@ -17,7 +15,7 @@ typedef bool (*filter_func)(const void *elem);  // for counters
 
 /// Lifecycle
 Array *arrayNew(size_t length);
-Array *arrayShallowCopy(ConstArray *array);
+Array *arrayShallowCopy(const Array *array);
 void arrayFreeWith(Array *array, deleter_func deleter);  // Frees payloads according to func
 void arrayFree(Array *array);  // Just frees itself
 
@@ -26,38 +24,37 @@ size_t arrayLength(Array *array);
 bool arrayIsEmpty(Array *array);
 
 /// Access (read-only to `void **payload[i]`)
-ConstArray *arrayConstIfy(Array *array);
 void *arrayGet(Array *array, size_t index);
-const void *arrayCGet(ConstArray *array, size_t index);
+const void *arrayCGet(const Array *array, size_t index);
 void *arrayFirst(Array *array);
-const void *arrayCFirst(ConstArray *array);
+const void *arrayCFirst(const Array *array);
 void *arrayLast(Array *array);
-const void *arrayCLast(ConstArray *array);
+const void *arrayCLast(const Array *array);
 
 // Change
 bool arraySet(Array *array, size_t index, void *data);
 
 /// Search and count
-size_t arrayIndexOf(ConstArray *array, filter_func key);
-size_t arrayCount(ConstArray *array, filter_func key);
-size_t arrayMinIdx(ConstArray *array, key_val_func key);
-size_t arrayMaxIdx(ConstArray *array, key_val_func key);
+size_t arrayIndexOf(const Array *array, filter_func key);
+size_t arrayCount(const Array *array, filter_func key);
+size_t arrayMinIdx(const Array *array, key_val_func key);
+size_t arrayMaxIdx(const Array *array, key_val_func key);
 
 
 /// Sorting (return NEW sorted array)
 // AVG: O(n²)
-Array *arrayBubbleSorted (ConstArray *array, key_val_func key);
-Array *arrayInsertionSorted(ConstArray *array, key_val_func key);
-Array *arraySelectionSorted(ConstArray *array, key_val_func key);
-Array *arrayCocktailShakerSorted(ConstArray *array, key_val_func key);
-Array *arrayGnomeSorted(ConstArray *array, key_val_func key);
+Array *arrayBubbleSorted (const Array *array, key_val_func key);
+Array *arrayInsertionSorted(const Array *array, key_val_func key);
+Array *arraySelectionSorted(const Array *array, key_val_func key);
+Array *arrayCocktailShakerSorted(const Array *array, key_val_func key);
+Array *arrayGnomeSorted(const Array *array, key_val_func key);
 // AVG: O(n log n)
-Array *arrayMergeSorted  (ConstArray *array, key_val_func key);
-Array *arrayHeapSorted(ConstArray *array, key_val_func key);
-Array *arrayTimSorted(ConstArray *array, key_val_func key);
-Array *arrayIntroSorted(ConstArray *array, key_val_func key);
+Array *arrayMergeSorted  (const Array *array, key_val_func key);
+Array *arrayHeapSorted(const Array *array, key_val_func key);
+Array *arrayTimSorted(const Array *array, key_val_func key);
+Array *arrayIntroSorted(const Array *array, key_val_func key);
 // AVG: O(n log² n)
-Array *arrayShellSorted(ConstArray *array, key_val_func key);
+Array *arrayShellSorted(const Array *array, key_val_func key);
 // AVG: O(n log n) ; WORST O(n²)
-Array *arrayQuickSorted  (ConstArray *array, key_val_func key);
+Array *arrayQuickSorted  (const Array *array, key_val_func key);
 
