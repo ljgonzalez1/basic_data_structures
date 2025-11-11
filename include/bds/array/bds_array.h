80cb062dc75438738fcbd2be3934ea3af0d4e611
Luis González <ljgonzalez@uc.cl>
2025-11-11 11:06:59 -0300

    ✨ feat(include/bds/array/bds_array.h): add Array struct and core functions for dynamic array management

--- include/bds/array/bds_array.h
+++ include/bds/array/bds_array.h
@@ -1,1 +1,55 @@
 #pragma once
+
+#include <stddef.h>   // size_t
+#include <stdbool.h>  // bool
+
+typedef struct bds_array {
+    void **payload;
+    size_t length;
+} Array;
+
+typedef struct datqapoint_wrapper {
+	void *value;
+} Wrapper
+
+// Key functions
+typedef int (*key_val_func)(const void *elem);
+typedef void (*deleter_func)(void *elem);
+typedef bool (*filter_func)(const void *elem);  // for counters
+
+/// Lifecycle
+Array *arrayNew(size_t length);
+Array *arrayShallowCopy(const Array *array);
+void arrayFreeWith(Array *array, deleter_func deleter);  // Frees payloads according to func
+void arrayFree(Array *array);  // Just frees itself
+
+/// Info
+size_t arraylength(Array *array);
+bool arrayIsEmpty(Array *array);
+
+/// Access (read-only to `void **payload[i]`)
+const Array *arrayConstIfy(*array);
+void *arrayGet(Array *array, size_t index);
+const void *arrayCGet(const Array *array, size_t index);
+void *arrayFirst(Array *array);
+const void *arrayCFirst(const Array *array);
+void *arrayLast(Array *array);
+const void *arrayCLast(const Array *array);
+
+// Change
+bool arraySet(Array *array, size_t index, void *data);
+
+/// Search and count
+size_t arrayIndexOf(const Array *array, filter_func key);
+size_t arrayCount(const Array *array, filter_func key);
+size_t arrayMinIdx(const Array *array, key_val_func key);
+size_t arrayMaxIdx(const Array *array, key_val_func key);
+
+
+/// Sorting (return NEW sorted array)
+Array *arrayBubbleSorted (const Array *array, key_val_func key);
+Array *arrayMergeSorted  (const Array *array, key_val_func key);
+Array *arrayInsertionSorted(const Array *array, key_val_func key);
+Array *arrayQuickSorted  (const Array *array, key_val_func key);
+Array *arrayMinHeapSorted(const Array *array, key_val_func key);
+Array *arrayMaxHeapSorted(const Array *array, key_val_func key);
