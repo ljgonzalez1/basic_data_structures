e997782f4a9e4cbc03d05ceeed120a6c0c7445cd
Luis González <ljgonzalez@uc.cl>
2025-11-11 11:25:27 -0300

    🎉 refactor(array): move sorting and searching functions to a new header file for better organization

--- include/bds/array/bds_array.h
+++ include/bds/array/bds_array.h
@@ -34,27 +34,3 @@ const void *arrayCLast(const Array *array);
 // Change
 bool arraySet(Array *array, size_t index, void *data);
 
-/// Search and count
-size_t arrayIndexOf(const Array *array, filter_func key);
-size_t arrayCount(const Array *array, filter_func key);
-size_t arrayMinIdx(const Array *array, key_val_func key);
-size_t arrayMaxIdx(const Array *array, key_val_func key);
-
-
-/// Sorting (return NEW sorted array)
-// AVG: O(n²)
-Array *arrayBubbleSorted (const Array *array, key_val_func key);
-Array *arrayInsertionSorted(const Array *array, key_val_func key);
-Array *arraySelectionSorted(const Array *array, key_val_func key);
-Array *arrayCocktailShakerSorted(const Array *array, key_val_func key);
-Array *arrayGnomeSorted(const Array *array, key_val_func key);
-// AVG: O(n log n)
-Array *arrayMergeSorted  (const Array *array, key_val_func key);
-Array *arrayHeapSorted(const Array *array, key_val_func key);
-Array *arrayTimSorted(const Array *array, key_val_func key);
-Array *arrayIntroSorted(const Array *array, key_val_func key);
-// AVG: O(n log² n)
-Array *arrayShellSorted(const Array *array, key_val_func key);
-// AVG: O(n log n) ; WORST O(n²)
-Array *arrayQuickSorted  (const Array *array, key_val_func key);
-
