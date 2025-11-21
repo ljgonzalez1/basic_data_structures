285c11154da659824623b4c56988cd3518a01551
Luis González <ljgonzalez@uc.cl>
2025-11-20 23:22:57 -0300

    ✨ feat(src/array/array_sort.c): implement skeleton for various sorting algorithms in array sorting module

---
+++ src/array/array_sort.c
@@ -0,0 +1,136 @@
+#include "../../include/bds/array/bds_array_core.h"
+#include "../../include/bds/array/bds_array_sort.h"
+
+/// Sorting (return NEW sorted array)
+Array *arrayBubbleSorted (const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayInsertionSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arraySelectionSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayCocktailShakerSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayGnomeSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+Array *arrayMergeSorted  (const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayHeapSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayTimSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+Array *arrayIntroSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+// AVG: O(n log² n)
+Array *arrayShellSorted(const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
+
+// AVG: O(n log n) ; WORST O(n²)
+Array *arrayQuickSorted  (const Array *array, key_val_func key) {
+    Array *sorted_array = arrayShallowCopy(array);
+    if (!sorted_array) return NULL;
+
+    // TODO: ===================
+    // TODO: SORT ALGORITHM HERE
+    // TODO: ===================
+
+    return sorted_array;
+}
+
