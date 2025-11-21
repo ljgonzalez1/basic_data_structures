fc8b0d315959c1929d7865f20b79bd18491c9105
Luis González <ljgonzalez@uc.cl>
2025-11-21 06:25:48 -0300

    ✨ feat(array_sort.c): implement gnome sort algorithm for sorting arrays efficiently

--- src/array/array_sort.c
+++ src/array/array_sort.c
@@ -139,6 +139,44 @@ void arrayCocktailShakerSort(Array *array, const key_val_func key) {
     } while (swapped);
 }
 
+void arrayGnomeSort(Array *array, const key_val_func key) {
+    // Similar to insertion sort
+    // Simpler
+    // If 2 elements are out of order, swap them and step back
+    // Else, step forward
+    // Repeat until the end of the array is reached
+    const size_t length = arrayLength(array);
+    if (length < 2) return;
+
+    size_t start = 0;
+
+    while (start < length - 1) {
+        const int curr_idx = (int)start;
+        const int next_idx = (int)start + 1;
+
+        const int curr_val = key(arrayGet(array, curr_idx));
+        const int next_val = key(arrayGet(array, next_idx));
+
+        // If in order, step forward
+        if (curr_val <= next_val) {
+            start++;
+
+        // If out of order, swap...
+        // ...and step back (if possible)...
+        // ...if not possible, we are at the start, just step forward.
+        } else {
+            swap(array, curr_idx, next_idx);
+
+            if (start > 0) {
+                start--;
+
+            } else {
+                start ++;
+            }
+        }
+    }
+}
+
 Array *arrayBubbleSorted (const Array *array, const key_val_func key) {
     Array *sorted_array = arrayShallowCopy(array);
     if (!sorted_array) return NULL;
@@ -178,13 +216,11 @@ Array *arrayCocktailShakerSorted(const Array *array, const key_val_func key) {
 }
 
 
-Array *arrayGnomeSorted(const Array *array, key_val_func key) {
+Array *arrayGnomeSorted(const Array *array, const key_val_func key) {
     Array *sorted_array = arrayShallowCopy(array);
     if (!sorted_array) return NULL;
 
-    // TODO: ===================
-    // TODO: SORTING ALGORITHM HERE
-    // TODO: ===================
+    arrayGnomeSort(sorted_array, key);
 
     return sorted_array;
 }
