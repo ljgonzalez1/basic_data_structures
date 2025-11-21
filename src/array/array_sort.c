18da5e1de532d494adf0237a290845c6bf186a79
Luis González <ljgonzalez@uc.cl>
2025-11-20 23:41:44 -0300

    🎨 refactor(array_sort): implement bubble sort algorithm for array sorting functionality

--- src/array/array_sort.c
+++ src/array/array_sort.c
@@ -1,14 +1,40 @@
 #include "../../include/bds/array/bds_array_core.h"
 #include "../../include/bds/array/bds_array_sort.h"
 
+static inline void swap(Array *array, size_t idx1, size_t idx2) {
+    void *temp = arrayGet(array, idx1);
+    arraySet(array, idx1, arrayGet(array, idx2));
+    arraySet(array, idx2, temp);
+}
+
 /// Sorting (return NEW sorted array)
 Array *arrayBubbleSorted (const Array *array, key_val_func key) {
     Array *sorted_array = arrayShallowCopy(array);
     if (!sorted_array) return NULL;
 
-    // TODO: ===================
-    // TODO: SORT ALGORITHM HERE
-    // TODO: ===================
+    size_t length = arrayLength(sorted_array);
+
+    if (length < 2) return sorted_array;
+
+    bool swapped;
+
+    do {
+        swapped = false;
+
+        for (size_t idx = 0; idx < length - 1; idx++) {
+            size_t next_idx = idx + 1;
+
+            int this_val = key(arrayGet(sorted_array, idx));
+            int next_val = key(arrayGet(sorted_array, next_idx));
+
+            if (this_val > next_val) {
+                swap(sorted_array, idx, next_idx);
+                swapped = true;
+            }
+        }
+
+        length--;  // Last element is in place, no need to check again.
+    } while (swapped);
 
     return sorted_array;
 }
