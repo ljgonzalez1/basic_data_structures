168b6394b604217b29d6a352ba8116d8adc58072
Luis González <ljgonzalez@uc.cl>
2026-02-15 17:19:32 -0300

    ✨ refactor(bds_stack_core.h): improve stack expansion logic and update push method for better memory management

--- include/bds/stack/bds_stack_core.h
+++ include/bds/stack/bds_stack_core.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "../bds_config.h"
-
 #include "../bds_types.h"
 #include "../array/bds_array.h"
 
 #include <stddef.h>   // size_t
 #include <stdbool.h>  // bool
+#include <stdlib.h>   // realloc
 
 typedef struct bds_stack {
     void **data;
@@ -17,13 +17,9 @@ typedef struct bds_stack {
 //// Lifecycle ////
 
 Stack *stackNew(size_t length);
-
 Stack *stackShallowCopy(const Stack *stack);
-
 void stackFreeWith(Stack *stack, deleter_func deleter);  // Frees payloads according to func
-
 void stackFree(Stack *stack);  // Just frees itself
-
 Stack *stackNewFromArray(const Array *array);  // Copies array data into stack; stack capacity >= array length
 
 //// Helper ////
@@ -32,15 +28,8 @@ static inline bool stackExists(const Stack *stack) {
     return !!stack;
 }
 
-
-static inline Stack *stackAutoExpand(Stack *stack) {
-    // "Greater than" SHOULD BE IMPOSSIBLE. Just overflow prevention + who knows what.
-    if (!stackExists(stack) || stackLength(stack) < stackCapacity(stack)) return stack;
-
-    size_t new_cap = (size_t)((double)stackCapacity(stack) * (STACK_AUTO_EXPAND_FACTOR + 1.0)) + 1;
-    Stack *expandedStack = realloc(stack, sizeof(void *) * new_cap);
-
-    return expandedStack ? (expandedStack->max_length = new_cap, expandedStack) : stack;
+static inline bool stackNeedsExpansion(const Stack *stack) {
+    return (stackExists(stack) && stack->top_idx >= stack->max_length);
 }
 
 /// Info ///
@@ -57,7 +46,6 @@ static inline bool stackIsEmpty(const Stack *stack) {
     return stackLength(stack) == 0;
 }
 
-
 //// Access (read-only to `void **data[i]`) ////
 
 static inline void *stackGet(const Stack *stack, const size_t index) {
@@ -65,35 +53,23 @@ static inline void *stackGet(const Stack *stack, const size_t index) {
 }
 
 static inline void *stackPeek(const Stack *stack) {
-    return stackEmpty(stack) ? NULL : stack->data[stack->top_idx - 1];
+    return stackIsEmpty(stack) ? NULL : stack->data[stack->top_idx - 1];
 }
 
-
 //// Change ////
+
+static inline Stack *stackAutoExpand(Stack *stack) {
+    if (!stackNeedsExpansion(stack)) return stack;
+    size_t new_cap = (size_t)((double)stack->max_length * (STACK_AUTO_EXPAND_FACTOR + 1.0)) + 1;
+    void **new_data = realloc(stack->data, sizeof(void *) * new_cap);
+    return new_data ? (stack->data = new_data, stack->max_length = new_cap, stack) : stack;
+}
 
 static inline void *stackPop(Stack *stack) {
     return stackIsEmpty(stack) ? NULL : stack->data[--stack->top_idx];
 }
 
-static inline bool stackPush(Stack *stack, void *data) {
-    if (!stackExists(stack)) return false;
-
-    if (stackLength(stack) == stackCapacity(stack)) {
-        Stack *expanded_stack = stackAutoExpand(stack);
-        if (!expanded_stack) return false;
-        stack = expanded_stack;
-    }
-
-    stack->data[stack->top_idx++] = data;
-    return true;
-}
-
-
-
-//// Change ////
-
-// sets array[index] = data
-static inline void arraySet(Array *array, const size_t index, void *data) {
-    if (!arrayExists(array) || index >= arrayLength(array)) return;
-    array->data[index] = data;
+static inline void *stackPush(Stack *stack, void *data) {
+    stack = stackAutoExpand(stack);
+    return stackExists(stack) ? (stack->data[stack->top_idx++] = data) : NULL;
 }
