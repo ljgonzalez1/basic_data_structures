3c4352680d9b166ad6b7fce0acd4a6f450f1a88b
Luis González <ljgonzalez@uc.cl>
2026-02-28 06:31:45 -0300

    ✨ feat(include/bds/list): update function signatures for listGet and listInsert to remove size_t type for index ✨ feat(src/list/sorting): implement merge sort algorithm for linked lists in list_merge_sort.c ✨ fix(include/bds/array): rename arrayCombSorted to arrayCombSort for consistency 🐛 fix(src/array/sorting): correct return statement in array_selection_sort.c to return sorted_array 🛠️ refactor(src/array/sorting): optimize gap calculation in array_comb_sort.c for clarity

--- include/bds/list/bds_list_sort.h
+++ include/bds/list/bds_list_sort.h
@@ -4,11 +4,7 @@
 
 /// Sorting (SAME sorted array)
 void listMergesort(List *list, key_val_func key);
-void listTimSort(List *list, key_val_func key);
-void listCountingSort(List *list, key_val_func key);
 
 /// Sorting (returns NEW sorted array)
 List *listMergeSorted(List *list, key_val_func key);
-List *listTimSorted(List *list, key_val_func key);
-List *listCountingSorted(List *list, key_val_func key);
 
